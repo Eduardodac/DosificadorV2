79b000314bbebcbcb5837fa053bbe19a42426f5e
EduardoDac <eduardo.dac@outlook.com>
2024-12-06 12:59:12 -0600

    Agrega funciones servo

---
+++ components/servo/servo.h
@@ -0,0 +1,7 @@
+#ifndef SERVO_H
+#define SERVO_H
+
+void servo_init(void);           // Función para inicializar el servomotor
+void servo_update_angle(int angle);  // Función para actualizar el ángulo del servomotor
+
+#endif // SERVO_H
